52d3ba56a7707d4b82c7e724337fcb546103f49d
Meredith Jones <mj19003@gmail.com>
2020-12-09 17:51:49 -0500

    updated with search fixes

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ $(document).ready(function(){
         //userCity, local variable. Feed parameter 
         // userCity to function WeatherApi so it can use it
         WeatherApi(userCity);
+        $(".SearchCity").val("");
     }
     //Create function to list the cities already searched 
     // and stored in the localStorage
@@ -65,22 +66,22 @@ $(document).ready(function(){
         console.log(userCity)
         //Build the URL we need to get the current weather information
         var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + userCity + "&appid=" + APIKey;
-
+        console.log(queryURL);
         // Here we run our AJAX call to the OpenWeatherMap API
         $.ajax({
             url: queryURL,
             method: "GET"
         })
-            // We store all of the retrieved data inside of an object called "response"
+            // We store all of the data inside of an object called "response"
             .then(function(response) {
-                console.log(response);
+                console.log ("the response" + response);
 
                 //Adding weather icons 
                 //create URLs to get icons from weather API
                 var CurrentIcon = "http://openweathermap.org/img/wn/"+response.list[0].weather[0].icon+"@2x.png";
                 var FirstDay = "http://openweathermap.org/img/wn/"+response.list[6].weather[0].icon+"@2x.png";
                 var SecondDay = "http://openweathermap.org/img/wn/"+response.list[14].weather[0].icon+"@2x.png";
-                var ThirdDAy = "http://openweathermap.org/img/wn/"+response.list[22].weather[0].icon+"@2x.png";
+                var ThirdDay = "http://openweathermap.org/img/wn/"+response.list[22].weather[0].icon+"@2x.png";
                 var FourthDay = "http://openweathermap.org/img/wn/"+response.list[30].weather[0].icon+"@2x.png";
                 var FithDay = "http://openweathermap.org/img/wn/"+response.list[38].weather[0].icon+"@2x.png";
         
@@ -88,19 +89,20 @@ $(document).ready(function(){
                 var IconMain = $('<img src=" '+ CurrentIcon +' "/>');
                 var IconOne = $('<img src=" '+ FirstDay +' "/>');
                 var IconTwo = $('<img src=" '+ SecondDay +' "/>');
-                var IconThree = $('<img src=" '+ ThirdDAy +' "/>');
+                var IconThree = $('<img src=" '+ ThirdDay +' "/>');
                 var IconFour = $('<img src=" '+ FourthDay +' "/>');
                 var IconFive = $('<img src=" '+ FithDay +' "/>');
 
                 //Current weather
-                //Data to populate CurrentWeather in DOM
+                //Data for adding to the html 
                 $("#TitleCity").text(response.city.name + "  (" + response.list[0].dt_txt.substr(0, 10) + ")").append(IconMain); // Use substr(0, 10 to only retrieve date and not time from the WeatherAPI)
                 
                 //Make sure that the tempurature displays in farenheight 
                 $("#Temp").text("Temperature: " + ((response.list[6].main.temp- 273.15) * 1.80 + 32).toFixed(2) + " F");
                 $("#Humidity").text("Humidity: " + response.list[6].main.humidity + " %");
                 $("#Wind").text("Wind Speed: " + response.list[6].wind.speed + " mph");
-                //For UVI index, create a variable that store the coordinates. 
+                
+                //For the  UVI index, create a variable that store the coordinates. 
                 //Use them with the UVindexAPI to retrieve on currentWeather class HTML.
                 var latitude = response.city.coord.lat;
                 var longitude = response.city.coord.lon;
@@ -108,31 +110,37 @@ $(document).ready(function(){
                 //Transfer the local variables to weatherUVI()
                 WeatherUVI(latitude, longitude)
 
-                //1st day = current weather. Info display from mid-day information
-                $("#Date1").text(response.list[6].dt_txt.substr(0, 10));
+                //First day current weather.
+                console.log(new Date().toLocaleDateString())
+                //response.list[6].dt_txt.substr(0, 10)
+                $("#Date1").text(new Date().toLocaleDateString());
                 $("#icon1").empty().append(IconOne);
                 $("#Temp1").text("Temp: " + ((response.list[6].main.temp - 273.15) * 1.80 + 32).toFixed(2) + " F");
-                $("#Humidity1").text("Hum.: " + response.list[6].main.humidity + " %");
-                //2nd day after current weather. Info display from mid-day information
+                $("#Humidity1").text("Humidity: " + response.list[6].main.humidity + " %");
+
+                //Second day after current weather. Info display from mid-day information
                 $("#Date2").text(response.list[14].dt_txt.substr(0, 10));
                 $("#icon2").empty().append(IconTwo);
                 $("#Temp2").text("Temp: " + ((response.list[14].main.temp - 273.15) * 1.80 + 32).toFixed(2) + " F");
-                $("#Humidity2").text("Hum: " + response.list[14].main.humidity + " %");
+                $("#Humidity2").text("Humidity: " + response.list[14].main.humidity + " %");
+
                 //3nd day after current weather. Info display from mid-day information
                 $("#Date3").text(response.list[22].dt_txt.substr(0, 10));
                 $("#icon3").empty().append(IconThree);
                 $("#Temp3").text("Temp: " + ((response.list[22].main.temp - 273.15) * 1.80 + 32).toFixed(2) + " F");
-                $("#Humidity3").text("Hum: " + response.list[22].main.humidity + " %");
+                $("#Humidity3").text("Humidity: " + response.list[22].main.humidity + " %");
+
                 //4nd day after current weather. Info display from mid-day information
                 $("#Date4").text(response.list[30].dt_txt.substr(0, 10));
                 $("#icon4").empty().append(IconFour);
                 $("#Temp4").text("Temp: " + ((response.list[30].main.temp - 273.15) * 1.80 + 32).toFixed(2) + " F");
-                $("#Humidity4").text("Hum: " + response.list[30].main.humidity + " %");
+                $("#Humidity4").text("Humidity: " + response.list[30].main.humidity + " %");
+
                 //5nd day after current weather. Info display from mid-day information
                 $("#Date5").text(response.list[38].dt_txt.substr(0, 10));
                 $("#icon5").empty().append(IconFive);
                 $("#Temp5").text("Temp: " + ((response.list[38].main.temp - 273.15) * 1.80 + 32).toFixed(2) + " F");
-                $("#Humidity5").text("Hum: " + response.list[38].main.humidity + " %");
+                $("#Humidity5").text("Humidity: " + response.list[38].main.humidity + " %");
                 
             });
     } // }closes WeatherAPI ()
@@ -151,22 +159,36 @@ $(document).ready(function(){
             .then(function(response1) {
                 console.log(typeof response1.current.uvi)
                 var UVI = response1.current.uvi;
+
+                
                 
                 //Print UVIndex
                 $("#UVIndex").text("UV Index: " + UVI);
                 
-                if (UVI <= 2.99) {                  
+                if (UVI <= 1.99) {                  
                     UVI = $("#UVIndex").css({"background-color": "lightgreen", "display": "inline", "padding": "1%", "border-radius": "10px"});
-                } else if (UVI >= 3 & UVI <= 5.99) {
+                } else if (UVI >= 2 & UVI <= 5.99) {
                     UVI = $("#UVIndex").css({"background-color": "yellow", "display": "inline", "padding": "1%", "border-radius": "10px"});
-                } else if (UVI >= 6 & UVI <= 7.99) {
+                } else if (UVI >= 4 & UVI <= 7.99) {
                     UVI = $("#UVIndex").css({"background-color": "orange", "display": "inline", "padding": "1%", "border-radius": "10px"});
-                } else if (UVI >= 8) {
+                } else if (UVI >= 6) {
                     UVI = $("#UVIndex").css({"background-color": "red", "display": "inline", "padding": "1%", "border-radius": "10px"});
                 };
 
             });
     } // Closes WeatherUVI()
+
+    //Example: Viewing the results of API call:
+    //  var "" = $(this).attr("data-name");
+    // var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+
+    // Creating an AJAX call for the specific content button being clicked
+    // $.ajax({
+    //   url: queryURL,
+    //   method: "GET"
+    // }).then(function(response) {
+    //   console.log(response);
+    //  }
 
     //function to activate click search on stored cities
     function clickOnCities() {
