5e617e7af1596aa9c55462341ec3da196bf563dd
Meredith Jones <mj19003@gmail.com>
2020-12-09 20:35:10 -0500

    Updated with readme images

--- script.js
+++ script.js
@@ -12,14 +12,13 @@ $(document).ready(function(){
     function GetInput(event) {
         event.preventDefault();
 
-        //create a variable to store the input
+        //create a variable to store the input from user searches
         var userCity = $(".SearchCity").val(); 
 
         //Create the array of cities that have been searched for
         Cities.push(userCity);
 
         //use JSON to make a string from the cities in the Cities Array
-
         localStorage.setItem("CitiesNames", JSON.stringify(Cities));
 
         //Display the cities that have been searched below the search bar
@@ -28,56 +27,55 @@ $(document).ready(function(){
         $("#CityList").append(SearchedCities);
 
         
-        //userCity, local variable. Feed parameter 
+        
         // userCity to function WeatherApi so it can use it
         WeatherApi(userCity);
         $(".SearchCity").val("");
     }
+
     //Create function to list the cities already searched 
-    // and stored in the localStorage
+    // and stored in localStorage
     function CitiesList() {
         //Convert the String into a JSON object
         Cities = JSON.parse(localStorage.getItem("CitiesNames"));
-        //If CitiesNames doesn't exist in localstorage, Cities will be null. 
-        // If Cities is null, Cities.length will throw an error. 
-        // If Cities = null, initialize Cities anyway.
-        if (Cities == null) {
-        Cities = [];
-        }
-        console.log(Cities);
+    
 
-        //Loop through the array of Cities
+        //Loop through the array of Cities created
         for (var i = 0; i < Cities.length; i++) {
             var CitiesToDisplay = Cities[i];
-            // I want to call them individually and then display in the list
-            // print the information in a list, print every name we have in local storage
+
+            // Call them individually and then display in the list from local storage
+            
             var List = $("<div>").text(CitiesToDisplay).addClass("Clickable"); 
-            //add class to add clickable function later
+            //add class to be able to add clickable function later on
             $("#CityList").append(List);
 
-            //Create an if function so that 
+            
         }
     }
     // API key to use with openweathermap
+    // Saving the key to a variable to that I don't have to paste it in every time it's used.
     var APIKey = "43150bac67a6b6bc9ffc3d398fb24e60";
 
-    //Generates current weather and 5 days weather
+    //Generating current weather and 5 day forecast
     function WeatherApi (userCity) {
         console.log(userCity)
-        //Build the URL we need to get the current weather information
+
+        // URL we need to get the current weather information
         var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + userCity + "&appid=" + APIKey;
         console.log(queryURL);
-        // Here we run our AJAX call to the OpenWeatherMap API
+
+        // AJAX call to the OpenWeatherMap API
         $.ajax({
             url: queryURL,
             method: "GET"
         })
-            // We store all of the data inside of an object called "response"
+            // Store all of the data inside of an object called "response"
             .then(function(response) {
                 console.log ("the response" + response);
 
                 //Adding weather icons 
-                //create URLs to get icons from weather API
+                //create URLs to get icons from Openweather API
                 var CurrentIcon = "http://openweathermap.org/img/wn/"+response.list[0].weather[0].icon+"@2x.png";
                 var FirstDay = "http://openweathermap.org/img/wn/"+response.list[6].weather[0].icon+"@2x.png";
                 var SecondDay = "http://openweathermap.org/img/wn/"+response.list[14].weather[0].icon+"@2x.png";
@@ -85,17 +83,17 @@ $(document).ready(function(){
                 var FourthDay = "http://openweathermap.org/img/wn/"+response.list[30].weather[0].icon+"@2x.png";
                 var FithDay = "http://openweathermap.org/img/wn/"+response.list[38].weather[0].icon+"@2x.png";
         
-                //Store the image in a variable to use below
+                //Store the weather icons in variables below
                 var IconMain = $('<img src=" '+ CurrentIcon +' "/>');
-                var IconOne = $('<img src=" '+ FirstDay +' "/>');
-                var IconTwo = $('<img src=" '+ SecondDay +' "/>');
-                var IconThree = $('<img src=" '+ ThirdDay +' "/>');
-                var IconFour = $('<img src=" '+ FourthDay +' "/>');
-                var IconFive = $('<img src=" '+ FithDay +' "/>');
+                var IconFirst = $('<img src=" '+ FirstDay +' "/>');
+                var IconSecond = $('<img src=" '+ SecondDay +' "/>');
+                var IconThird = $('<img src=" '+ ThirdDay +' "/>');
+                var IconFourth = $('<img src=" '+ FourthDay +' "/>');
+                var IconFifth = $('<img src=" '+ FithDay +' "/>');
 
                 //Current weather
                 //Data for adding to the html 
-                $("#TitleCity").text(response.city.name + "  (" + response.list[0].dt_txt.substr(0, 10) + ")").append(IconMain); // Use substr(0, 10 to only retrieve date and not time from the WeatherAPI)
+                $("#MainCity").text(response.city.name + "  (" + response.list[0].dt_txt.substr(0, 10) + ")").append(IconMain); // Use substr(0, 10 to only retrieve date and not time from the WeatherAPI)
                 
                 //Make sure that the tempurature displays in farenheight 
                 $("#Temp").text("Temperature: " + ((response.list[6].main.temp- 273.15) * 1.80 + 32).toFixed(2) + " F");
@@ -114,44 +112,45 @@ $(document).ready(function(){
                 console.log(new Date().toLocaleDateString())
                 //response.list[6].dt_txt.substr(0, 10)
                 $("#Date1").text(new Date().toLocaleDateString());
-                $("#icon1").empty().append(IconOne);
+                $("#icon1").empty().append(IconFirst);
                 $("#Temp1").text("Temp: " + ((response.list[6].main.temp - 273.15) * 1.80 + 32).toFixed(2) + " F");
                 $("#Humidity1").text("Humidity: " + response.list[6].main.humidity + " %");
 
                 //Second day after current weather. Info display from mid-day information
                 $("#Date2").text(response.list[14].dt_txt.substr(0, 10));
-                $("#icon2").empty().append(IconTwo);
+                $("#icon2").empty().append(IconSecond);
                 $("#Temp2").text("Temp: " + ((response.list[14].main.temp - 273.15) * 1.80 + 32).toFixed(2) + " F");
                 $("#Humidity2").text("Humidity: " + response.list[14].main.humidity + " %");
 
                 //3nd day after current weather. Info display from mid-day information
                 $("#Date3").text(response.list[22].dt_txt.substr(0, 10));
-                $("#icon3").empty().append(IconThree);
+                $("#icon3").empty().append(IconThird);
                 $("#Temp3").text("Temp: " + ((response.list[22].main.temp - 273.15) * 1.80 + 32).toFixed(2) + " F");
                 $("#Humidity3").text("Humidity: " + response.list[22].main.humidity + " %");
 
                 //4nd day after current weather. Info display from mid-day information
                 $("#Date4").text(response.list[30].dt_txt.substr(0, 10));
-                $("#icon4").empty().append(IconFour);
+                $("#icon4").empty().append(IconFourth);
                 $("#Temp4").text("Temp: " + ((response.list[30].main.temp - 273.15) * 1.80 + 32).toFixed(2) + " F");
                 $("#Humidity4").text("Humidity: " + response.list[30].main.humidity + " %");
 
                 //5nd day after current weather. Info display from mid-day information
                 $("#Date5").text(response.list[38].dt_txt.substr(0, 10));
-                $("#icon5").empty().append(IconFive);
+                $("#icon5").empty().append(IconFifth);
                 $("#Temp5").text("Temp: " + ((response.list[38].main.temp - 273.15) * 1.80 + 32).toFixed(2) + " F");
                 $("#Humidity5").text("Humidity: " + response.list[38].main.humidity + " %");
                 
             });
-    } // }closes WeatherAPI ()
+    } 
 
     //Using latitude and longitude with get UVindex and display on Currentweather 
     function WeatherUVI(latitude,longitude) {  
                  
-        //Build the URL we need to get the UVI information
+        //Build the URL we need to get the UVindex information
         var accessURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=hourly,daily&appid=" + APIKey;
 
-        // Here we run our AJAX call to the OpenWeatherMap API
+        
+        // Running the AJAX call to the OpenWeatherMap API
         $.ajax({
             url: accessURL,
             method: "GET"
@@ -162,21 +161,21 @@ $(document).ready(function(){
 
                 
                 
-                //Print UVIndex
+                //Showing the UVIndex
                 $("#UVIndex").text("UV Index: " + UVI);
                 
                 if (UVI <= 1.99) {                  
-                    UVI = $("#UVIndex").css({"background-color": "lightgreen", "display": "inline", "padding": "1%", "border-radius": "10px"});
+                    UVI = $(".card-body").css({"background-color": "lightgreen", "display": "inline", "padding": "1%", "border-radius": "10px"});
                 } else if (UVI >= 2 & UVI <= 5.99) {
-                    UVI = $("#UVIndex").css({"background-color": "yellow", "display": "inline", "padding": "1%", "border-radius": "10px"});
+                    UVI = $(".card-body").css({"background-color": "yellow", "display": "inline", "padding": "1%", "border-radius": "10px"});
                 } else if (UVI >= 4 & UVI <= 7.99) {
-                    UVI = $("#UVIndex").css({"background-color": "orange", "display": "inline", "padding": "1%", "border-radius": "10px"});
+                    UVI = $(".card-body").css({"background-color": "orange", "display": "inline", "padding": "1%", "border-radius": "10px"});
                 } else if (UVI >= 6) {
-                    UVI = $("#UVIndex").css({"background-color": "red", "display": "inline", "padding": "1%", "border-radius": "10px"});
+                    UVI = $(".card-body").css({"background-color": "red", "display": "inline", "padding": "1%", "border-radius": "10px"});
                 };
 
             });
-    } // Closes WeatherUVI()
+    } 
 
     //Example: Viewing the results of API call:
     //  var "" = $(this).attr("data-name");
@@ -201,11 +200,18 @@ $(document).ready(function(){
     }
     WeatherApi("Philadelphia"); 
 
-    //Call|Execute the function
+    //Call the function
     $(".fa").on("click", GetInput);
     $(document).on("click", ".Clickable", clickOnCities);
 
-    //Call Function whe the page loads.
+    // $(document).ready(function() {
+    //     $("fa").keyup(function(event) {
+    //       if (event.which === 13) {
+    //         $("#submit").click();
+    //       }
+    //     });
+
+    //Call Function when the page loads.
     CitiesList();
 
 
